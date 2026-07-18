26c42427980105b31663efbbf2d1af8c8061ac3d
akbarsilohon <akbarsilohon@gmail.com>
2026-07-19 04:29:16 +0700

    Push

---
+++ wp-next.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Wordpress to Next Js
+ * Version: 1.1.0
+ * Plugin URI: https://silohon.com
+ * Description: Plugin pribadi pengembangan Wordpress to Next Js
+ * Author: Akbar Silohon
+ * Author URI: https://github.com/akbarsilohon
+ */
+
